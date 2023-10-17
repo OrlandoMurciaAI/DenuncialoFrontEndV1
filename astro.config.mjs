0878fcf9acbf40bd7d0e4eec730e5bb0ae33cae7
orlando murcia <orlandomurcia.perdomo@gmail.com>
2023-10-16 22:00:35 -0500

    adding react component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   //output: "server",
-  integrations: [tailwind(),mdx()]
+  integrations: [tailwind(),mdx(), react()]
 });
