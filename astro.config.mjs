9b9085ce88445353a3c5928d80d96d7fdd49b91a
orlando murcia <orlandomurcia.perdomo@gmail.com>
2023-10-17 23:07:04 -0500

    adding svelte component as social media after footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   //output: "server",
-  integrations: [tailwind(),mdx(), react(), vue()]
+  integrations: [tailwind(),mdx(), react(), vue(), svelte()]
 });
--- src/components/SecondHeader.tsx
+++ src/components/SecondHeader.tsx
@@ -8,10 +8,12 @@ const Header = ({ title }: Props) => {
     return ( 
         <div className="mx-auto max-w-screen-xl p-4 py-8"> 
             <h1 className="mb-4">
+                <a href="/">
                 <span className="font-extrabold text-transparent text-4xl bg-clip-text 
                 bg-gradient-to-r from-purple-400 to-pink-600">
                     {title}
                 </span>
+                </a>
             </h1>
         </div> 
     );
